9639ebd55bf089191159a1cc43c9d7bd9de5584f
rf-gul <rf-gul@pop-os.localdomain>
2024-07-24 18:06:46 +0500

    signup and login issue still persists but the ui is visible on the screen

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,83 +1,17 @@
-// import conf from "../conf/conf.js";
-
-// import { Client, Account, ID } from "appwrite";
-
-// export class AuthService {
-//   client = new Client();
-//   account;
-
-//   constructor() {
-//     this.client
-//       .setEndpoint(conf.appWriteUrl)
-//       .setProject(conf.appWriteProjectId);
-//     this.account = new Account(this.client);
-//   }
-
-//   async createAccount({ email, password, name }) {
-//     // the account creation function can fail thats why we put it in try and catch block
-//     try {
-//       const userAccount = await this.account.create(
-//         ID.unique(),
-//         email,
-//         password,
-//         name
-//       );
-
-//       if (userAccount) {
-//         //call another function mostly the login function will be here
-//         return this.login({ email, password });
-//       } else {
-//         return userAccount;
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   async login({ email, password }) {
-//     try {
-//       return await this.account.createEmailSession(email, password);
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-//   async getCurrentUser() {
-//     try {
-//       return this.account.get();
-//     } catch (error) {
-//       console.log("appwrite service :: getCurrentUser :: error " + error);
-//     }
-
-//     return null;
-//   }
-//   async logout() {
-//     try {
-//       await this.account.deleteSessions();
-//     } catch (error) {
-//       console.log("appwrite service error :: logout ::" + error);
-//     }
-//   }
-// }
-
-// const authService = new AuthService(); //object of AuthService class
-// export default AuthService;
-
-
-
 import conf from "../conf/conf.js";
 import { Client, Account, ID } from "appwrite";
-
 export class AuthService {
   client = new Client();
   account;
-
   constructor() {
+    console.log(conf.appWriteUrl)
     this.client
       .setEndpoint(conf.appWriteUrl)
       .setProject(conf.appWriteProjectId);
     this.account = new Account(this.client);
   }
-
   async createAccount({ email, password, name }) {
+    // the account creation function can fail thats why we put it in try and catch block
     try {
       const userAccount = await this.account.create(
         ID.unique(),
@@ -86,43 +20,39 @@ export class AuthService {
         name
       );
       if (userAccount) {
-        // call another method
+        //call another function mostly the login function will be here
         return this.login({ email, password });
       } else {
         return userAccount;
       }
     } catch (error) {
-      throw error;
+      console.log(error);
     }
   }
-
   async login({ email, password }) {
     try {
-      return await this.account.createEmailPasswordSession(email, password);
+      return await this.account.createEmailSession(email, password);
     } catch (error) {
-      throw error;
+      console.log(error);
     }
   }
-
   async getCurrentUser() {
     try {
       return await this.account.get();
     } catch (error) {
-      console.log("Appwrite serive :: getCurrentUser :: error", error);
-    }
 
+      console.error("appwrite service :: getCurrentUser :: error " + error);
+    }
     return null;
   }
-
+  
   async logout() {
     try {
       await this.account.deleteSessions();
     } catch (error) {
-      console.log("Appwrite serive :: logout :: error", error);
+      console.log("appwrite service error :: logout ::" + error);
     }
   }
 }
-
-const authService = new AuthService();
-
+const authService = new AuthService(); //object of AuthService class
 export default authService;
